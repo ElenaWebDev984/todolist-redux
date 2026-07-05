8671f815fd358b454cb7d2ac28c18233260e1ace
ElenaWebDev984 <badalova.dev.984@gmail.com>
2026-07-05 21:47:34 +0200

    refactoring changeTodolistTitleAC

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -4,9 +4,11 @@ import {createAction} from "@reduxjs/toolkit";
 
 const initialState: Todolist[] = []
 
-export const deleteTodolistAC = createAction<{id: string}>('todolists/deleteTodolist') // TODO Параметры: сначала название редьюсера, потом что будет делать этот action
-// export const deleteTodolistAC = (id: string) => {
-//     return {type: 'delete_todolist', payload: { id }} as const
+export const deleteTodolistAC = createAction<{id: string}>('todolists/deleteTodolist')
+
+export const changeTodolistTitleAC = createAction<{id: string, title: string}>('todolists/changeTodolistTitle')
+// export const changeTodolistTitleAC = (payload: {id: string, title: string}) => {
+//     return {type: 'change_todolist_title', payload} as const
 // }
 
 export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
@@ -35,9 +37,6 @@ export const createTodolistAC = (title: string) => {
   return {type: 'create_todolist', payload: { title, id: v1() }} as const
 }
 
-export const changeTodolistTitleAC = (payload: {id: string, title: string}) => {
-  return {type: 'change_todolist_title', payload} as const
-}
 
 export const changeTodolistFilterAC = (payload: {id: string, filter: FilterValues}) => {
   return {type: 'change_todolist_filter', payload} as const
