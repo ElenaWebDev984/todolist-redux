d06bb7094147b6b53c00cd0bf349b874a5e64367
ElenaWebDev984 <badalova.dev.984@gmail.com>
2026-07-07 13:53:41 +0200

    refactoring

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -1,5 +1,5 @@
-import type {Task, TasksState} from '../app/App.tsx'
-import {createTodolistAC,  deleteTodolistAC} from './todolists-reducer'
+import type {TasksState} from '../app/App.tsx'
+import {createTodolistAC, deleteTodolistAC} from './todolists-reducer'
 import {createAction, createReducer, nanoid} from "@reduxjs/toolkit";
 
 export const deleteTaskAC = createAction<{ todolistId: string, taskId: string }>('tasks/deleteTask')
@@ -35,25 +35,3 @@ export const tasksReducer = createReducer(initialState, (builder) => {
       })
 })
 
-export const tasksReducer2 = (state: TasksState = initialState, action: Actions): TasksState => {
-  switch (action.type) {
-    case "change_task_title": {
-      return {
-        ...state,
-        [action.payload.todolistId]: state[action.payload.todolistId].map(task => task.id === action.payload.taskId ? {...task, title: action.payload.title} : task)
-      }
-    }
-  }
-}
-
-
-export type DeleteTaskAction = ReturnType<typeof deleteTaskAC>
-export type CreateTaskAction = ReturnType<typeof createTaskAC>
-export type ChangeTaskStatusAction = ReturnType<typeof changeTaskStatusAC>
-export type ChangeTaskTitleAction = ReturnType<typeof changeTaskTitleAC>
-
-type Actions =
-    | DeleteTaskAction
-    | CreateTaskAction
-    | ChangeTaskStatusAction
-    | ChangeTaskTitleAction
